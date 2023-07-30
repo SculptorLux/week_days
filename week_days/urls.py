98c19e56a41a1535541303f60f546668f3965eb7
SculptorLux <alexanisin@gmail.com>
2023-07-30 15:55:13 +0300

    Week_days after 2.4

---
+++ week_days/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views as views_week_days
+
+urlpatterns = [
+    path('monday/', views_week_days.monday),
+    path('tuesday/', views_week_days.tuesday),
+]
