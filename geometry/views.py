b8fe43955d2757c0b553e013e53f0bda332fb9d2
SculptorLux <alexanisin@gmail.com>
2023-08-10 14:07:14 +0300

    2.8 Redirect Url в Django Давайте вспомним приложение «geometry». Необходимо добавить три роута
        get_rectangle_area/<int:width>/<int:height>
        get_square_area/<int:width>
        get_circle_area/<int:radius>
    
    В представлении views.py сделайте перенаправление на соответствующий имеющийся из того задания роут
    
    http://127.0.0.1:8000/calculate_geometry/get_rectangle_area/10/5
    должен перенаправить на
    http://127.0.0.1:8000/calculate_geometry/rectangle/10/5
    
    http://127.0.0.1:8000/calculate_geometry/get_square_area/10
    должен перенаправить на
    http://127.0.0.1:8000/calculate_geometry/square/10
    
    http://127.0.0.1:8000/calculate_geometry/get_circle_area/4
    должен перенаправить на
    http://127.0.0.1:8000/calculate_geometry/circle/4

--- geometry/views.py
+++ geometry/views.py
@@ -1,5 +1,5 @@
 import math
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from math import pi
 
 
@@ -16,3 +16,15 @@ def get_square_area(request, width: int):
 
 def get_circle_area(request, radius: int):
     return HttpResponse(f"Площадь круга с радиусом {radius} = {(radius * pi) ** 2}")
+
+
+def rectangle_redirect(request, width: int, height: int):
+    return HttpResponseRedirect(f"/calculate_geometry/rectangle/{width}/{height}/")
+
+
+def square_redirect(request, width: int):
+    return HttpResponseRedirect(f"/calculate_geometry/square/{width}/")
+
+
+def circle_redirect(request, radius: int):
+    return HttpResponseRedirect(f"/calculate_geometry/circle/{radius}")
