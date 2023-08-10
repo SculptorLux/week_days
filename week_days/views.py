cb12d33e732e4f4db456ca60061c1f7ec2a83a9a
SculptorLux <alexanisin@gmail.com>
2023-08-10 12:16:10 +0300

    2.8 Redirect Url в Django
    В приложении week_days  выполните перенаправление с роута с числовым значением на роут с названием дня
    
    /todo_week/1 -> перенаправляет на -> /todo_week/monday
    /todo_week/7 -> перенаправляет на -> /todo_week/sunday
    /todo_week/21 -> Неверный номер дня - 21

--- week_days/views.py
+++ week_days/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
+
+dic_week_day = {'monday': 'Понедельник!',
+                'tuesday': 'Вторник!',
+                'wednesday': 'Среда!',
+                'thursday': 'Четверг!',
+                'friday': 'Пятница!',
+                'saturday': 'Суббота!',
+                'sunday': 'Воскресенье!'}
 
 
 def get_info_about_day(request, day):
-    if day == 'monday':
-        return HttpResponse('На понедельник нет планов')
-    elif day == 'tuesday':
-        return HttpResponse('Во вторник учим python')
-    else:
-        return HttpResponseNotFound('Прости, про твой день ничего не знаю')
+    description = dic_week_day.get(day, None)
+    if description:
+        return HttpResponse(dic_week_day[day])
+    return HttpResponseNotFound(f'Нет такого дня в моей неделе {day}')
 
 
 def get_info_by_number_of_day(request, number_of_day: int):
-    if number_of_day <=7:
-        return HttpResponse(f'Сегодня {number_of_day} день недели')
+    if number_of_day <= 7:
+        days_list = list(dic_week_day)
+        day_x = days_list[number_of_day - 1]
+        return HttpResponseRedirect(f'/todo_week/{day_x}')
     return HttpResponse(f'Неверный номер дня - {number_of_day}')
